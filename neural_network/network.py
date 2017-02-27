7496f8189d270db3934c8367c70138f5ec2aeca5
Spaceman1701 <fc.spaceman@gmail.com>
2017-02-27 14:52:15 -0700

    work on data strucutre for network

--- neural_network/network.py
+++ neural_network/network.py
@@ -5,19 +5,19 @@ class Network:
     def __init__(self, sizes):
         self.sizes = sizes
         self.num_layers = len(sizes)
-        self.weights = np.array([[[np.random.uniform() for weight in range(sizes[layer + 1])]  # weights[layer][node_fom][node_to]
+        self.weights = np.array([[[np.random.uniform() for weight in range(sizes[layer])]  # weights[layer][node_fom][node_to]
                                 for node in range(size)]
-                                 for layer, size in enumerate(sizes[:-1])])
+                                 for layer, size in enumerate(sizes[1:])])
         self.biases = np.array([[np.random.uniform() for bias in range(size)] for size in sizes])  # bias[layer][node]
+
+        ##print(self.weights)
 
     def calc_activation(self, layer, prev_activation):
         layer_weights = self.weights[layer]
-        print(len(layer_weights))
         output = []
         for node in layer_weights:
-
             output.append(np.dot(node, prev_activation))
-        print(len(output))
+        print("output activation: {0}, {1}, bias = {2}".format(len(output), output, self.biases))
         return output + self.biases
 
     def calc_sigmoid(self, activation):
@@ -27,11 +27,15 @@ class Network:
         for layer in range(self.num_layers):
             inputs = self.calc_sigmoid(self.calc_activation(layer, inputs))
 
+    def print_weights(self):
+        print("Network Weights:")
+        for layer_num, layer in enumerate(self.weights):
+            print("layer {0}: ".format(layer_num), end="")
+            print(layer)
 
-#l = [3, 2, 1]
-#print(l[:-1])
-n = Network([1, 3, 1])
-n.feed_forward([1])
+
+n = Network([3, 1, 2])
+n.feed_forward([1, 1, 1])
 
 
 
